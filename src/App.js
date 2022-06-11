1b37176f782369218bee00f931e2480f85c58187
Kettengeschwindigkeit <666damned999@mail.ru>
2022-06-11 13:50:56 +0300

    lesson-7_what is Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Technologies />
     </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        <a href='#'>Home</a>
+        <a href='#'>News Feed</a>
+        <a href='#'>Messages</a>
+      </ul>
+    </div>
+  );
+}
+
+const Technologies = () => {
+  return (
+      <div>
+        <ul>
+          <li>html</li>
+          <li>css</li>
+          <li>js</li>
+          <li>react</li>
+        </ul>
+      </div>
   );
 }
 
