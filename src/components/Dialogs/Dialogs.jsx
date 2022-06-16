7f3712ba8e387482ca33242287dfe673d606dde1
Kettengeschwindigkeit <666damned999@mail.ru>
2022-06-16 16:36:05 +0300

    lesson_22 - route exact

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Dialogs.module.css'
-
 
 
 const Dialogs = (props) => {
@@ -8,29 +8,29 @@ const Dialogs = (props) => {
         <div className={styles.dialogs}>
             <div className={styles.dialogsItems}>
                 <div className={styles.dialogs + ' ' + styles.active}>
-                    Jesse Pinkman
+                    <NavLink to='/dialogs/1'>Jesse Pinkman</NavLink>
                 </div>
                 <div className={styles.dialogs}>
-                    Badger
+                    <NavLink to='/dialogs/2'>Badger</NavLink>
                 </div>
                 <div className={styles.dialogs}>
-                    Skinny Pete
+                    <NavLink to='/dialogs/3'>Skinny Pete</NavLink>
                 </div>
                 <div className={styles.dialogs}>
-                    Krazy-8
+                    <NavLink to='/dialogs/4'>Krazy-8</NavLink>
                 </div>
                 <div className={styles.dialogs}>
-                    Tuco
+                    <NavLink to='/dialogs/5'>Tuco</NavLink>
                 </div>
                 <div className={styles.dialogs}>
-                    Jane
+                    <NavLink to='/dialogs/6'>Jane</NavLink>
                 </div>
             </div>
             <div className={styles.messages}>
-                    <div className={styles.message}>Hey! What up beyotch!</div>
-                    <div className={styles.message}>Yo</div>
-                    <div className={styles.message}>Yo</div>
-                </div>
+                <div className={styles.message}>Hey! What up beyotch!</div>
+                <div className={styles.message}>Yo</div>
+                <div className={styles.message}>Yo</div>
+            </div>
         </div>
     )
 }
