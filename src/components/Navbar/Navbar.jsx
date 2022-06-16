855bfed2e2c04f95772d05d5f74129f2c98a1684
Kettengeschwindigkeit <666damned999@mail.ru>
2022-06-16 13:22:06 +0300

    lesson_19 - route, browser-router, routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
 import styles from './Navbar.module.css'
 
-// let styles =  {
-//     'nav': 'Navbar_nav_3ou9Q',
-//     'item': 'Navbar_nav_3qaF3'
-// }
 
 const Navbar = () => {
     return (
         <nav className={styles.nav}>
             <div className={styles.item}>
-                <a>Profile</a>
+                <a href='/profile'>Profile</a>
             </div>
             <div className={`${styles.item} ${styles.active}`}>
-                <a>Messages</a>
+                <a href='dialogs'>Messages</a>
             </div>
             <div className={styles.item}>
-                <a>News</a>
+                <a href='news'>News</a>
             </div>
             <div className={styles.item}>
-                <a>Music</a>
+                <a href='music'>Music</a>
             </div>
             <div className={styles.item}>
-                <a>Settings</a>
+                <a href='settings'>Settings</a>
             </div>
         </nav>
     )
