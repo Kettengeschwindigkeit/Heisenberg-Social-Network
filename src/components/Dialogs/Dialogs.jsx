ab6865d56aa44b16b2d9c7e87543dbae1f10e6b4
Kettengeschwindigkeit <666damned999@mail.ru>
2022-06-16 15:32:34 +0300

    lesson_21 - dialogs page layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,8 +5,32 @@ import styles from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialogs + ' ' + styles.active}>
+                    Jesse Pinkman
+                </div>
+                <div className={styles.dialogs}>
+                    Badger
+                </div>
+                <div className={styles.dialogs}>
+                    Skinny Pete
+                </div>
+                <div className={styles.dialogs}>
+                    Krazy-8
+                </div>
+                <div className={styles.dialogs}>
+                    Tuco
+                </div>
+                <div className={styles.dialogs}>
+                    Jane
+                </div>
+            </div>
+            <div className={styles.messages}>
+                    <div className={styles.message}>Hey! What up beyotch!</div>
+                    <div className={styles.message}>Yo</div>
+                    <div className={styles.message}>Yo</div>
+                </div>
         </div>
     )
 }
