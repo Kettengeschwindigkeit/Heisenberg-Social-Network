eeba97482a79a4871b37d938395266cf53f15f53
Kettengeschwindigkeit <666damned999@mail.ru>
2022-06-21 11:29:32 +0300

    lesson_31 - onClick, ref, VirtualDOM

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,16 +5,23 @@ import Post from './Post/Post';
 
 const MyPosts = (props) => {
 
+    let newPostElement = React.createRef()
+
+    let addPost = () => {
+        let text = newPostElement.current.value
+        alert(text)
+    }
+
     let postsElements = props.posts.map(post => <Post message={post.message} likesCount={post.likesCount} />)
 
     return (
         <div className={styles.postsBlock}>
             <h3>My posts</h3>
             <div>
-                <textarea></textarea>
+                <textarea ref={ newPostElement }></textarea>
             </div>
             <div>
-                <button>Add post</button>
+                <button onClick={ addPost }>Add post</button>
             </div>
             <div className={styles.posts}>
                 {postsElements}
