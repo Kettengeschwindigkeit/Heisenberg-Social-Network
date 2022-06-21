3735e9538ab2b3d8c65c363066a119984706722a
Kettengeschwindigkeit <666damned999@mail.ru>
2022-06-21 09:40:04 +0300

    lesson_29 - wrapping data up in state

--- src/index.js
+++ src/index.js
@@ -2,27 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import state from './redux/state'
 
 
-let posts = [
-    { id: 1, message: 'Hey! What up beyotch!', likesCount: 12 },
-    { id: 2, message: 'Yo yo yo!!', likesCount: 11 }
-]
-
-let dialogs = [
-    { id: 1, name: 'Jesse Pinkman' },
-    { id: 2, name: 'Badger' },
-    { id: 3, name: 'Skinny Pete' },
-    { id: 4, name: 'Combo' },
-    { id: 5, name: 'Tuco' },
-    { id: 6, name: 'Jane' }
-]
-
-let messages = [
-    { id: 1, message: 'Hey! What up beyotch!' },
-    { id: 2, message: 'Yo!' },
-    { id: 3, message: 'Yo!' },
-    { id: 4, message: 'Yo!' }
-]
-
-ReactDOM.render(<App posts={posts} dialogs={dialogs} messages={messages} />, document.getElementById('root'));
+ReactDOM.render(<App state={state} />, document.getElementById('root'));
---
+++ src/redux/state.js
@@ -0,0 +1,29 @@
+
+let state = {
+
+    profilePage: {
+        posts: [
+            { id: 1, message: 'Hey! What up beyotch!', likesCount: 12 },
+            { id: 2, message: 'Yo yo yo!!', likesCount: 11 }
+        ]
+    },
+
+    dialogsPage: {
+        dialogs: [
+            { id: 1, name: 'Jesse Pinkman' },
+            { id: 2, name: 'Badger' },
+            { id: 3, name: 'Skinny Pete' },
+            { id: 4, name: 'Combo' },
+            { id: 5, name: 'Tuco' },
+            { id: 6, name: 'Jane' }
+        ],
+        messages: [
+            { id: 1, message: 'Hey! What up beyotch!' },
+            { id: 2, message: 'Yo!' },
+            { id: 3, message: 'Yo!' },
+            { id: 4, message: 'Yo!' }
+        ]
+    }
+}
+
+export default state
