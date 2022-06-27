302e442975838308e65cd416dc32ed350c2e672d
Kettengeschwindigkeit <666damned999@mail.ru>
2022-06-27 10:58:11 +0300

    lesson_33 - adding post, FLUX-concept

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,6 +10,7 @@ const MyPosts = (props) => {
     let addPost = () => {
         let text = newPostElement.current.value
         props.addPost(text)
+        newPostElement.current.value = ''
     }
 
     let postsElements = props.posts.map(post => <Post message={post.message} likesCount={post.likesCount} />)
