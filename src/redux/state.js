95666ec444f971c8efe133bbac7b0faada9b2967
Kettengeschwindigkeit <666damned999@mail.ru>
2022-06-29 08:13:22 +0300

    lesson_37 - store, state, OOP, refactoring

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,84 +1,91 @@
-let rerenderEntireTree = () => {
-    console.log('state was changed')
-}
 
-let state = {
+let store = {
 
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Hey! What up beyotch!', likesCount: 12 },
-            { id: 2, message: 'Yo yo yo!!', likesCount: 11 }
-        ],
-        newPostText: 'it-kamasutra.com'
-    },
+    _state: {
 
-    dialogsPage: {
-
-        dialogs: [
-            { id: 1, name: 'Badger', avatar: 'https://quizswish.com/wp-content/uploads/2020/08/badger-bb-311x311.jpg' },
-            { id: 2, name: 'Skinny Pete', avatar: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/breaking-bad/e/e2/Skinny_Pete.jpg' },
-            { id: 3, name: 'Combo', avatar: 'https://render.fineartamerica.com/images/rendered/default/front/spiral-notebook/images/artworkimages/medium/2/combo-breaking-bad-joseph-oland.jpg?&targetx=-317&targety=-1&imagewidth=1534&imageheight=961&modelwidth=680&modelheight=961&backgroundcolor=6C8CAB&orientation=0&producttype=spiralnotebook' },
-            { id: 4, name: 'Emilio', avatar: 'https://m.media-amazon.com/images/M/MV5BMTI1OTQ1MjEzM15BMl5BanBnXkFyZXN1bWU@._V1_FMjpg_UX1000_.jpg' },
-            { id: 5, name: 'Krazy-8', avatar: 'https://www.personality-database.com/profile_images/64557.png' },
-            { id: 6, name: 'Tuco', avatar: 'http://thx-trailer.com/replica/Breaking_bad/grill.jpg' },
-            { id: 7, name: 'Jane', avatar: 'https://i.pinimg.com/736x/c0/85/5f/c0855f79316c897231219ba0fcb36372.jpg' }
-        ],
-
-        messages: [
-            { id: 1, message: 'Hey! What up beyotch!' },
-            { id: 2, message: 'Yo!' },
-            { id: 3, message: 'Yo!' },
-            { id: 4, message: 'Yo!' }
-        ],
-        newMessageText: 'it-kamasutra.com'
-    },
-
-    navbarPage: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Hey! What up beyotch!', likesCount: 12 },
+                { id: 2, message: 'Yo yo yo!!', likesCount: 11 }
+            ],
+            newPostText: 'it-kamasutra.com'
+        },
     
-        friends: [
-            { id: 1, name: 'Badger', avatar: 'https://quizswish.com/wp-content/uploads/2020/08/badger-bb-311x311.jpg' },
-            { id: 2, name: 'Skinny Pete', avatar: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/breaking-bad/e/e2/Skinny_Pete.jpg' },
-            { id: 3, name: 'Combo', avatar: 'https://render.fineartamerica.com/images/rendered/default/front/spiral-notebook/images/artworkimages/medium/2/combo-breaking-bad-joseph-oland.jpg?&targetx=-317&targety=-1&imagewidth=1534&imageheight=961&modelwidth=680&modelheight=961&backgroundcolor=6C8CAB&orientation=0&producttype=spiralnotebook' },
-        ]
+        dialogsPage: {
+    
+            dialogs: [
+                { id: 1, name: 'Badger', avatar: 'https://quizswish.com/wp-content/uploads/2020/08/badger-bb-311x311.jpg' },
+                { id: 2, name: 'Skinny Pete', avatar: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/breaking-bad/e/e2/Skinny_Pete.jpg' },
+                { id: 3, name: 'Combo', avatar: 'https://render.fineartamerica.com/images/rendered/default/front/spiral-notebook/images/artworkimages/medium/2/combo-breaking-bad-joseph-oland.jpg?&targetx=-317&targety=-1&imagewidth=1534&imageheight=961&modelwidth=680&modelheight=961&backgroundcolor=6C8CAB&orientation=0&producttype=spiralnotebook' },
+                { id: 4, name: 'Emilio', avatar: 'https://m.media-amazon.com/images/M/MV5BMTI1OTQ1MjEzM15BMl5BanBnXkFyZXN1bWU@._V1_FMjpg_UX1000_.jpg' },
+                { id: 5, name: 'Krazy-8', avatar: 'https://www.personality-database.com/profile_images/64557.png' },
+                { id: 6, name: 'Tuco', avatar: 'http://thx-trailer.com/replica/Breaking_bad/grill.jpg' },
+                { id: 7, name: 'Jane', avatar: 'https://i.pinimg.com/736x/c0/85/5f/c0855f79316c897231219ba0fcb36372.jpg' }
+            ],
+    
+            messages: [
+                { id: 1, message: 'Hey! What up beyotch!' },
+                { id: 2, message: 'Yo!' },
+                { id: 3, message: 'Yo!' },
+                { id: 4, message: 'Yo!' }
+            ],
+            newMessageText: 'it-kamasutra.com'
+        },
+    
+        navbarPage: {
+        
+            friends: [
+                { id: 1, name: 'Badger', avatar: 'https://quizswish.com/wp-content/uploads/2020/08/badger-bb-311x311.jpg' },
+                { id: 2, name: 'Skinny Pete', avatar: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/breaking-bad/e/e2/Skinny_Pete.jpg' },
+                { id: 3, name: 'Combo', avatar: 'https://render.fineartamerica.com/images/rendered/default/front/spiral-notebook/images/artworkimages/medium/2/combo-breaking-bad-joseph-oland.jpg?&targetx=-317&targety=-1&imagewidth=1534&imageheight=961&modelwidth=680&modelheight=961&backgroundcolor=6C8CAB&orientation=0&producttype=spiralnotebook' },
+            ]
+        }
+    
+    },
+
+    getState() {
+        return this._state
+    },
+
+    _callSubscriber() {
+        console.log('state was changed')
+    },
+
+    addPost() {
+        let newPost = {
+            id: 3,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._callSubscriber(this._state)
+    },
+
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText
+        this._callSubscriber(this._state)
+    },
+
+    addMessage() {
+        let newMessage = {
+            id: 5,
+            message: this._state.dialogsPage.newMessageText
+        }
+        this._state.dialogsPage.messages.push(newMessage)
+        this._state.dialogsPage.newMessageText = ''
+        this._callSubscriber(this._state)
+    },
+    
+    updateNewMessageText (newText) {
+        this._state.dialogsPage.newMessageText = newText
+        this._callSubscriber(this._state)
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer
     }
+}   
 
-}
-
-window.state = state
-
-export const addPost = () => {
-    let newPost = {
-        id: 3,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    }
-    state.profilePage.posts.push(newPost)
-    state.profilePage.newPostText = ''
-    rerenderEntireTree(state)
-}
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText
-    rerenderEntireTree(state)
-}
-
-export const addMessage = () => {
-    let newMessage = {
-        id: 5,
-        message: state.dialogsPage.newMessageText
-    }
-    state.dialogsPage.messages.push(newMessage)
-    state.dialogsPage.newMessageText = ''
-    rerenderEntireTree(state)
-}
-
-export const updateNewMessageText = (newText) => {
-    state.dialogsPage.newMessageText = newText
-    rerenderEntireTree(state)
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer
-}
-
-export default state
+export default store
+window.sore = store
