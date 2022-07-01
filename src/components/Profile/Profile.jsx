f23b51d79a323a1385dfe168b0d4d1656c8aa64b
Kettengeschwindigkeit <666damned999@mail.ru>
2022-07-01 07:32:19 +0300

    lesson_43 - container component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import MyPosts from './MyPosts/MyPosts';
-import styles from './Profile.module.css'
+import MyPostsContainer from './MyPosts/MyPostsContainer';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 
@@ -8,7 +7,7 @@ const Profile = (props) => {
     return (
         <div>
             <ProfileInfo />
-            <MyPosts posts={props.profilePage.posts} newPostText={props.profilePage.newPostText} dispatch={props.dispatch} />
+            <MyPostsContainer store={props.store} />
         </div>
     )
 }
