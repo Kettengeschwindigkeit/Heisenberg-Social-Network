027e9c80811274d31b8c39a7da2dd9e9e6501f56
Kettengeschwindigkeit <666damned999@mail.ru>
2022-07-06 23:41:15 +0300

    lesson_55 - pagination

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,7 +1,8 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
-
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
 
 let initialState = {
     users: [
@@ -9,7 +10,10 @@ let initialState = {
         // { id: 2, followed: true, name: 'Jesse Pinkman', status: "Hey! What up, beyotch!", location: { state: 'New-Mexico', city: 'Albuquerque' }, photoUrl: 'https://i.insider.com/5d9f3f5183486904582ee506?width=700' },
         // { id: 3, followed: false, name: 'Hank Schrader', status: "It's easy money - 'Til we catch you", location: { state: 'New-Mexico', city: 'Albuquerque' }, photoUrl: 'https://sun6-19.userapi.com/o8HyngNuGhAPvWUKLGPSZQdBP4-uhKvuE6wV7Q/kQLS-1ENC8U.jpg' },
         // { id: 4, followed: false, name: 'Walter J. White', status: "Yo", location: { state: 'New-Mexico', city: 'Albuquerque' }, photoUrl: 'https://pbs.twimg.com/profile_images/3015927487/2a891f966fc3d0605593200f79ad8739_400x400.jpeg' }
-     ]
+     ],
+     pageSize: 5,
+     totalUsersCount: 20,
+     currentPage: 1
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -40,6 +44,12 @@ const usersReducer = (state = initialState, action) => {
 
         case SET_USERS:
             return { ...state, users: action.users }
+        
+        case SET_CURRENT_PAGE:
+            return { ...state, currentPage: action.currentPage }
+
+        case SET_TOTAL_USERS_COUNT:
+            return { ...state, totalUsersCount: action.count }
 
         default:
             return state
@@ -50,5 +60,7 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userId) => ({ type: FOLLOW, userId })
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
 export const setUsersAC = (users) => ({ type: SET_USERS, users })
+export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage })
+export const setTotalUsersCountAC = (totalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, count: totalUsersCount })
 
 export default usersReducer 
